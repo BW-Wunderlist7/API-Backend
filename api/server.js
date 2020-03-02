d4ec6070b830d6b42b9bc961f4c57ed2b41adf85
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-03-02 13:33:14 -0500

    env

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
+// require("dotenv").config();
 
 // make sure that helmet is hiding the powered by
 server.use(helmet());
