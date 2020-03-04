cfd5982cb697fbd5c9e53c97772a5cd2d3211587
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-03-04 15:54:56 -0500

    production

--- db/db-config.js
+++ db/db-config.js
@@ -1,4 +1,4 @@
 const knex = require("knex");
 // change development to production for heroku
-const configOptions = require("../knexfile").development;
+const configOptions = require("../knexfile").production;
 module.exports = knex(configOptions);
